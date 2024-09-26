3a2f5aa13269af0a66aa8f7f46324af0c71bcebe
artlands <lijie0416@gmail.com>
2024-09-26 15:00:56 -0500

    add timer

--- test/xbgas_isa/eblb.c
+++ test/xbgas_isa/eblb.c
@@ -22,6 +22,7 @@ extern int  __xbrtime_asm_get_npes();
 extern void __xbrtime_wait_bulk_comp();
 
 int main( int argc, char** argv ) {
+  struct __kernel_timespec s, e;
 
   int id    = __xbrtime_asm_get_id();
   int npes  = __xbrtime_asm_get_npes();
@@ -50,9 +51,14 @@ int main( int argc, char** argv ) {
 
   // Load the source address
   if( id == 0 ) {
+    rev_clock_gettime( 0, &s );
+    // printf( "PE %d: before buld load, clock time = %lu \n", id, s.tv_nsec );
     // Remote bulk load
     asm volatile( " eblb %0, %1, %2, %3 \n\t " : : "r"( dest ), "r"( src2 ), "r"( nelem ), "r"( sizeof( int8_t ) ) );
     __xbrtime_wait_bulk_comp();
+    rev_clock_gettime( 0, &e );
+    // printf( "PE %d: after buld load, clock time = %lu \n", id, e.tv_nsec );
+    printf( "PE %d: Remote bulk load time: %lu ns\n", id, ( e.tv_nsec - s.tv_nsec ) );
     for( int i = 0; i < nelem; i++ ) {
       printf( "PE %d: dest[%d] = 0x%02x\n", id, i, dest[i] );
       assert( dest[i] == src2[i] );
